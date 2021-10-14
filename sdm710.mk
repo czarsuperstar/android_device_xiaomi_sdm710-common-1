4a7a39db0ad646e27ed76c98faea4012973c22a9
TH779 <i@779.moe>
2021-10-14 15:37:47 +0000

    sdm710-common: Uprev fingerprint HAL to 2.3

--- sdm710.mk
+++ sdm710.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.3.vendor \
+		android.hardware.drm@1.4-service.clearkey
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
